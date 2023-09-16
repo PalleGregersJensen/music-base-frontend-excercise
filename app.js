9e6964b7fc3afcb81d16d089a397fec3bd44c9d6
Elvasfar <brondby_1964@hotmail.com>
2023-09-16 07:59:12 +0200

    frontend+search

--- app.js
+++ app.js
@@ -1,119 +1,176 @@
 "use strict";
 
+import { getAlbumData, getArtistData, getTrackData } from "./rest-service.js";
+
 window.addEventListener("load", initApp);
 
 let albums = [];
 let artists = [];
 let tracks = [];
 
+let filteredAlbums = [];
+let filteredArtists = [];
+let filteredTracks = [];
+
 async function initApp() {
   console.log("app running!");
-  document.querySelector("#searchfield").addEventListener("keyup", searchArtist);
-  albums = await getJasonData();
-  console.log(albums);
-  showAlbumsOnWebsite(albums);
-  artists = albums;
-  console.log(artists);
-  showArtistsOnWebsite(artists);
-  tracks = [].concat(
-    albums[0].tracks,
-    albums[1].tracks,
-    albums[2].tracks,
-    albums[3].tracks,
-    albums[4].tracks,
-    albums[5].tracks,
-    albums[6].tracks,
-    albums[7].tracks,
-    albums[8].tracks,
-    albums[9].tracks
+  document.querySelector("#input-search").addEventListener("keyup", searchDatabase);
+  albums = await getAlbumData();
+  artists = await getArtistData();
+  tracks = await getTrackData();
+
+  showAlbums(albums);
+  showArtists(artists);
+  showTracks(tracks);
+}
+
+function showAlbums(filteredAlbums) {
+  document.querySelector("#albums").insertAdjacentHTML(
+    "beforeend",
+    `
+            <h1>Albums</h1>
+  <table id="albums-table">
+    <thead>
+      <tr>
+        <th>Cover</th>
+        <th>Title</th>
+        <th>Releasedate</th>
+        <th>Tracks</th>
+      </tr>
+    </thead>
+    <tbody id="albums-table-body"></tbody>
+  </table>
+  `
   );
-  console.log(albums[0].tracks);
-  console.log(tracks);
-  showTracksOnWebsite(tracks);
-}
 
-// get Json-data
-async function getJasonData() {
-  const response = await fetch("albums.json");
-  console.log(response);
-  const data = await response.json();
-  console.log(data);
-  return data;
-}
+  const albumsTable = document.querySelector("#albums-table-body");
 
-// show albums on website
-function showAlbumsOnWebsite(albumList) {
-  document.querySelector("#albums").innerHTML = "Albums:";
-  for (const album of albumList) {
-    const albumHtml = /*html*/ `<br>${album.title}`;
-    document.querySelector("#albums").insertAdjacentHTML("beforeend", albumHtml);
+  for (const album of filteredAlbums) {
+    const row = albumsTable.insertRow();
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+    const cell4 = row.insertCell(3);
+
+    cell1.innerHTML = `<img src="${album.albumCover}" />`;
+    cell2.textContent = album.albumTitle;
+    cell3.textContent = album.releaseDate;
+    cell4.textContent = album.numberOfTracks;
   }
 }
 
 // show artists on website
-function showArtistsOnWebsite(albumList) {
-  document.querySelector("#artists").innerHTML = "Artists:";
-  for (const album of albumList) {
-    const artistHtml = /*html*/ `<br>${album.artist}`;
-    document.querySelector("#artists").insertAdjacentHTML("beforeend", artistHtml);
+function showArtists(filteredArtists) {
+  document.querySelector("#artists").insertAdjacentHTML(
+    "beforeend",
+    `
+            <h1>Artists</h1>
+  <table id="artists-table">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Birthdate</th>
+        <th>Genres</th>
+        <th>Website</th>
+        <th>Image</th>
+        <th>Number of Albums</th>
+      </tr>
+    </thead>
+    <tbody id="artists-table-body"></tbody>
+  </table>
+  `
+  );
+
+  const artistsTable = document.querySelector("#artists-table-body");
+
+  for (const artist of filteredArtists) {
+    const row = artistsTable.insertRow();
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+    const cell4 = row.insertCell(3);
+    const cell5 = row.insertCell(4);
+    const cell6 = row.insertCell(5);
+
+    cell1.textContent = artist.name;
+    cell2.textContent = artist.birthdate;
+    cell3.textContent = artist.genres;
+    cell4.textContent = artist.website;
+    cell5.innerHTML = `<img src="${artist.image}" />`;
+    cell6.textContent = artist.numberOfAlbums;
   }
 }
 
-// show artists on website
-function showTracksOnWebsite(trackList) {
-  document.querySelector("#tracks").innerHTML = "Tracks:";
-  for (const track of trackList) {
-    const tracksHtml = /*html*/ `<br>${track}`;
-    document.querySelector("#tracks").insertAdjacentHTML("beforeend", tracksHtml);
+// show tracks on website
+function showTracks(filteredTracks) {
+  document.querySelector("#tracks").insertAdjacentHTML(
+    "beforeend",
+    `
+            <h1>Tracks</h1>
+  <table id="tracks-table">
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Duration</th>
+      </tr>
+    </thead>
+    <tbody id="tracks-table-body"></tbody>
+  </table>
+  `
+  );
+
+  const tracksTable = document.querySelector("#tracks-table-body");
+
+  for (const track of filteredTracks) {
+    const row = tracksTable.insertRow();
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+
+    cell1.textContent = track.trackName;
+    cell2.textContent = track.duration;
   }
 }
 
-// Search artists
-function searchArtist() {
-  const inputInSearchfield = document.querySelector("#searchfield").value;
-  console.log(inputInSearchfield);
-  const resultOfSearch = checkResultOfSearch(inputInSearchfield);
-    const resultOfSearchAlbums = checkResultOfSearchAlbums(inputInSearchfield);
-    const resultOfSearchTracks = checkResultOfSearchTracks(inputInSearchfield);
-  console.log(resultOfSearch);
-  showArtistsOnWebsite(resultOfSearch);
-  showAlbumsOnWebsite(resultOfSearchAlbums);
-  showTracksOnWebsite(resultOfSearchTracks);
+// Search artists+tracks+albums
+function searchDatabase(event) {
+  const value = event.target.value;
+
+  filteredArtists = searchArtists(value);
+  filteredAlbums = searchAlbums(value);
+  filteredTracks = searchTracks(value);
+
+  // Clear the existing content of the tables
+  clearTableContent();
+
+  // Update the tables with the filtered results
+  showArtists(filteredArtists);
+  showAlbums(filteredAlbums);
+  showTracks(filteredTracks);
 }
 
-
-// Check tracks
-function checkResultOfSearchTracks(inputInSearchfield) {
-  const result = tracks.filter(filterByName);
-  console.log(result);
-  return result;
-
-  function filterByName(input) {
-    const inputLowerCase = inputInSearchfield.toLowerCase();
-    return input.toLowerCase().includes(inputLowerCase);
-  }
+function clearTableContent() {
+  // Clear the content of the artists, albums, and tracks tables
+  document.querySelector("#artists").innerHTML = "";
+  document.querySelector("#albums").innerHTML = "";
+  document.querySelector("#tracks").innerHTML = "";
+}
+function searchArtists(searchValue) {
+  searchValue = searchValue.toLowerCase();
+  const results = artists.filter((artist) => artist.name.toLowerCase().includes(searchValue));
+  console.log("Artists results:", results); // Log the search results
+  return results;
 }
 
-// check artists
-function checkResultOfSearch(inputInSearchfield) {
-  const result = artists.filter(filterByName);
-  console.log(result);
-  return result;
-
-  function filterByName(input) {
-    const inputLowerCase = inputInSearchfield.toLowerCase();
-    return input.artist.toLowerCase().includes(inputLowerCase);
-  }
+function searchAlbums(searchValue) {
+  searchValue = searchValue.toLowerCase();
+  const results = albums.filter((album) => album.albumTitle.toLowerCase().includes(searchValue));
+  console.log("Albums results:", results); // Log the search results
+  return results;
 }
 
-// check albums
-function checkResultOfSearchAlbums(inputInSearchfield) {
-  const result = artists.filter(filterByName);
-  console.log(result);
-  return result;
-
-  function filterByName(input) {
-    const inputLowerCase = inputInSearchfield.toLowerCase();
-    return input.title.toLowerCase().includes(inputLowerCase);
-  }
+function searchTracks(searchValue) {
+  searchValue = searchValue.toLowerCase();
+  const results = tracks.filter((track) => track.trackName.toLowerCase().includes(searchValue));
+  console.log("Tracks results:", results); // Log the search results
+  return results;
 }
---
+++ rest-service.js
@@ -0,0 +1,28 @@
+// get Json-data
+async function getAlbumData() {
+  const response = await fetch("/data/albums.json");
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+// get Json-data
+async function getTrackData() {
+  const response = await fetch("/data/tracks.json");
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+// get Json-data
+async function getArtistData() {
+  const response = await fetch("/data/artists.json");
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+export { getAlbumData, getArtistData, getTrackData };
